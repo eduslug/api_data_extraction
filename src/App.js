bce28d132536837ae40bd47820b223475e58c9ed
Eduardo <eduslug93@gmail.com>
2022-11-01 12:17:35 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import api from './api';
 
 function App() {
 
-	return(
+	const [posts, setPost] = useState([]);
+	const [loding, setloding] = useState(true);
+	useEffect(() => {
+		api.get('list_posts').then((response) => {
+			setPost(response.data.posts);
+			setloding(false)
+			console.log('Deu certo!')
+		}).catch(() => {
+			console.log('ERRO');
+		})
+
+	}, [])
+
+	// loding ? <h1>carregando</h1>:<h1>falha</h1>
+
+	if (loding) {
+		return (
+			<div className='loanding' class="loadingio-spinner-reload-e14ksbs1rqu"><div class="ldio-zzf8n5vtvqd">
+				<div><div></div><div></div><div></div></div>
+			</div></div>
+		)
+	}
+	return (
+
 		<div className="app">
 
 			<div className="cards">
 
-				<div className="card" >
-					<div className="card-body" >
-						<h1>Título do meu post</h1>
-						<div className="line"></div>
-						<h2>Conteudo da minha postagem, conteúdo da minha postagem</h2>
-					</div>
-				</div>
-			
-			</div>
+				{posts.map((post, key) => {
+					return (
+						<div className="card" key={key}>
+							<div className="card-body" >
+								<h1>{post.title}</h1>
+								<div className="line"></div>
+								<h2>{post.content}</h2>
+							</div>
+						</div>
+					)
+				})}
 
+			</div>
 		</div>
 	)
 
